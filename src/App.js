124f804f568025cbd2f2ca719481140ea5399fb1
unknown <tamiloreolaniyi@gmail.com>
2022-06-12 22:04:30 +0100

    UI complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
-import Home from './components/home/Home'
+import Home from './pages/home/Home'
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Register/>
     </div>
   );
 }
